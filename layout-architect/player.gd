1339298b258ff5611b424eda692be64a1aed3fb6
DiRasher04 <veegres04@mail.ru>
2026-03-06 12:29:36 +0300

    Первый коммит: добавил проект

---
+++ layout-architect/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+# Как быстро персонаж бегает
+@export var speed: float = 350.0
+@onready var animation = $AnimatedSprite2D
+
+func _physics_process(delta):
+	# Получаем ввод с клавиатуры
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	# Применяем скорость
+	velocity = input_direction * speed
+	# Двигаем персонажа
+	move_and_slide()
+	if input_direction.length() > 0:  # Если персонаж двигается
+		animation.play("walk")  # Замени "walk" на имя твоей анимации
+	else:  # Если стоит
+		animation.play("idle")  # Замени "idle" на имя твоей анимации покоя
